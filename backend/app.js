e8dcd4746b0a5e41f20cabcfff09bd3f5dc47fdb
Raj0603 <rajsrivastav0603@gmail.com>
2023-12-21 14:51:07 +0530

    mobile view updates for dashboard

--- backend/app.js
+++ backend/app.js
@@ -33,10 +33,10 @@ app.use("/api/v1",payment)
 
 app.use(errorMiddleware)
 
-app.use(express.static(path.join(__dirname, "../frontend/build")))
+// app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+// })
 
 module.exports = app
